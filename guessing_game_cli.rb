c18ede0c690e3ea2dd9e23df731da434e9431c9c
Radniel Padron <radnielpadron@gmail.com>
2020-02-19 00:25:52 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,6 +4,9 @@ def run_guessing_game
   random_num = rand(6) + 1
   puts "Please guess a number between 1 and 6"
   num = gets.chomp
+  if num == "exit"
+    puts "Goodbye!"
+  end
   # to_exit = gets.chomp
   guessing_num = num.to_i
   
